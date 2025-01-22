6f633f028efe94a03cf5e95d780e363cdee82bd0
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2025-01-22 14:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ def goal_tracking():
 if __name__ == '__main__':
     if not gemini_api_key:
         raise ValueError("Please set the GOOGLE_API_KEY environment variable")
-    app.run(debug=False)
+    app.run(debug=False, port=8000)
